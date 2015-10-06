dffdd2ac3fd278d871822404f44d38c60af3da73
Jan Aagaard <jaagaard@247e.com>
2015-10-06 20:53:54 +0200

    Canvas and hexagons size based on window size.

--- WebApplication/app/main.ts
+++ WebApplication/app/main.ts
@@ -3,6 +3,7 @@ module Muep {
 
     export class Main {
         constructor() {
+            this.setCanvasSize();
             Settings.initialize();
             Main.game = new Game();
             Main.canvas = new Canvas(Main.game);
@@ -10,5 +11,13 @@ module Muep {
 
         private static canvas: Canvas;
         private static game: Game;
+
+        private setCanvasSize() {
+            const minSize = Math.min(window.innerWidth, window.innerHeight);
+            const canvasSize = `${minSize}px`;
+            const canvasElement = document.getElementById(Settings.canvasId);
+            canvasElement.style.width = canvasSize;
+            canvasElement.style.height = canvasSize;
+        }
     }
 }
