06504c5cc4517d343c9533b8362ffa5fcbe03d10
Jan Aagaard <jaagaard@247e.com>
2015-10-20 15:50:20 +0200

    New units are placed on the players.

--- BusinessLogic/Player.cs
+++ BusinessLogic/Player.cs
@@ -1,4 +1,5 @@
-﻿using System.Net;
+﻿using System.Collections.Generic;
+using System.Net;
 using Newtonsoft.Json;
 
 namespace CocaineCartels.BusinessLogic
@@ -10,11 +11,15 @@ namespace CocaineCartels.BusinessLogic
             Color = color;
             IpAddress = ipAddress;
             UserAgent = userAgent;
+
+            NewUnits = new Unit[0];
         }
 
         /// <summary>The color also identifies a player, so use this property the player ID.</summary>
         public readonly string Color;
 
+        public readonly IEnumerable<Unit> NewUnits;
+            
         [JsonIgnore]
         public readonly IPAddress IpAddress;
 
