5f2618a01f149808f5239e1ad78eda91e1de08f6
Jan Aagaard <jaagaard@247e.com>
2015-10-22 05:01:15 +0200

    Fixed bootstrapping of JavaScript code.

--- WebApplication/app/cell.ts
+++ WebApplication/app/cell.ts
@@ -1,14 +1,7 @@
 module CocaineCartels {
     "use strict";
 
-    enum CellState {
-        Neutral,
-        DropAllowed,
-        DropAllowedAndHovered
-    }
-
     export class Cell {
-        /** Call initializeUnits for each cell after all cells have been initialized. */
         constructor(
             cellData: ICell
         ) {
