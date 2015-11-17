ca880c9a5a2f28c77638af4e1ef5c6de74a38abe
Jan Aagaard <jaagaard@247e.com>
2015-11-17 19:03:32 +0100

    Using SourceTree to commit the right files.

---
+++ BusinessLogic/Settings.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+
+namespace CocaineCartels.BusinessLogic
+{
+    public class Settings
+    {
+        [JsonProperty] public const int NewUnitPerCellsControlled = 3 + 2 + 1 + 1;
+        [JsonProperty] public const int GridSize = 2;
+        [JsonProperty] public const int MovesPerTurn = 5;
+        [JsonProperty] public const int NewUnitsPerTurn = 3;
+        [JsonProperty] public const int NumberOfStartingUnits = 3;
+        [JsonProperty] public const int PointsToWinTheGame = 50;
+    }
+}
